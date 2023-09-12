b4f59a04d0c7277ac75e5c3fc29e785115194096
NOU <125733144+N0ULife@users.noreply.github.com>
2023-09-12 15:34:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.button-30').addEventListener('click', () =>{
+    document.querySelector('.button').classList.toggle('active');
+    document.querySelector('.content').classList.toggle('active');
+})
